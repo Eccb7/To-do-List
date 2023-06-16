d2b930c65107e70d8de85739fdd14ce3c0aa58c8
Briton Otieno <ojwangbriton@gmail.com>
2023-06-16 13:07:58 +0300

    Add src files to have a default array which it iterates over

--- src/index.js
+++ src/index.js
@@ -5,20 +5,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const addTaskButton = document.getElementById('addTaskButton');
   const todoList = document.getElementById('todo-list');
 
-  addTaskButton.addEventListener('click', () => {
-    const taskText = taskInput.value;
-    if (taskText.trim() !== '') {
+  const tasks = [
+    { description: 'Task 1', completed: false, index: 1 },
+    { description: 'Task 2', completed: true, index: 2 },
+    { description: 'Task 3', completed: false, index: 3 },
+  ];
+
+  function renderTasks() {
+    // Clear existing tasks
+    todoList.innerHTML = '';
+
+    // Sort tasks by index
+    tasks.sort((a, b) => a.index - b.index);
+
+    // Create and append list items for each task
+    tasks.forEach((task) => {
       const taskItem = document.createElement('li');
 
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
+      checkbox.checked = task.completed;
       taskItem.appendChild(checkbox);
 
       const taskLabel = document.createElement('label');
-      taskLabel.innerText = taskText;
+      taskLabel.innerText = task.description;
       taskItem.appendChild(taskLabel);
 
       todoList.appendChild(taskItem);
+    });
+  }
+
+  // Render tasks on page load
+  renderTasks();
+
+  addTaskButton.addEventListener('click', () => {
+    const taskText = taskInput.value;
+    if (taskText.trim() !== '') {
+      const newTask = {
+        description: taskText,
+        completed: false,
+        index: tasks.length + 1,
+      };
+      tasks.push(newTask);
+      renderTasks();
       taskInput.value = '';
     }
   });
