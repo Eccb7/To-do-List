32c9a72ab759de7cdaa1bede96ec209c8849d508
Briton Otieno <ojwangbriton@gmail.com>
2023-06-17 19:03:02 +0300

    Update js file

--- src/index.js
+++ src/index.js
@@ -1,90 +1,10 @@
+// index.js
+
 import './style.css';
-
-// Retrieve tasks from local storage
-const getTasksFromStorage = () => {
-  const tasksJSON = localStorage.getItem('tasks');
-  return tasksJSON ? JSON.parse(tasksJSON) : [];
-};
-
-// Save tasks to local storage
-const saveTasksToStorage = (tasks) => {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
-
-// Edit a task description
-const editTaskDescription = (index, newDescription) => {
-  const tasks = getTasksFromStorage();
-  let renderTasks;
-  const task = tasks.find((task) => task.index === index);
-  if (task) {
-    task.description = newDescription;
-    saveTasksToStorage(tasks);
-    renderTasks();
-  }
-};
-
-// Delete a task
-const deleteTask = (index) => {
-  let renderTasks;
-  let tasks = getTasksFromStorage();
-  tasks = tasks.filter((task) => task.index !== index);
-  tasks.forEach((task, i) => {
-    task.index = i + 1;
-  });
-  saveTasksToStorage(tasks);
-  renderTasks();
-};
-
-// Render tasks
-const renderTasks = () => {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = '';
-
-  const tasks = getTasksFromStorage();
-  tasks.forEach((task) => {
-    const taskItem = document.createElement('li');
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
-    taskItem.appendChild(checkbox);
-
-    const taskDescription = document.createElement('span');
-    taskDescription.className = 'task-description';
-    taskDescription.innerText = task.description;
-    taskItem.appendChild(taskDescription);
-
-    const editButton = document.createElement('button');
-    editButton.className = 'edit-button';
-    editButton.innerHTML = '&#9998;';
-    taskItem.appendChild(editButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete-button';
-    const deleteIcon = document.createElement('span');
-    deleteIcon.className = 'delete-icon';
-    deleteButton.appendChild(deleteIcon);
-    taskItem.appendChild(deleteButton);
-
-    todoList.appendChild(taskItem);
-
-    checkbox.addEventListener('change', () => {
-      task.completed = checkbox.checked;
-      saveTasksToStorage(tasks);
-    });
-
-    editButton.addEventListener('click', () => {
-      const newDescription = ('Enter a new description:', task.description);
-      if (newDescription !== null) {
-        editTaskDescription(task.index, newDescription);
-      }
-    });
-
-    deleteButton.addEventListener('click', () => {
-      deleteTask(task.index);
-    });
-  });
-};
+import {
+  getTasksFromStorage, saveTasksToStorage, /* editTaskDescription, deleteTask, */
+} from './modules/taskModule.js';
+import { renderTasks } from './modules/renderModule.js';
 
 // Add a new task
 const addTask = (description) => {
@@ -96,7 +16,7 @@ const addTask = (description) => {
   };
   tasks.push(newTask);
   saveTasksToStorage(tasks);
-  renderTasks();
+  renderTasks(tasks);
 };
 
 // Clear completed tasks
@@ -107,7 +27,7 @@ const clearCompletedTasks = () => {
     task.index = i + 1;
   });
   saveTasksToStorage(tasks);
-  renderTasks();
+  renderTasks(tasks);
 };
 
 // Event listener for adding a task
@@ -133,7 +53,6 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.reload();
   });
 
-  renderTasks();
+  const tasks = getTasksFromStorage();
+  renderTasks(tasks);
 });
-
-export default renderTasks;
