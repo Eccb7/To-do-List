c5fe0b211afffc3135eb38eb14bd8636a2848a24
Briton Otieno <ojwangbriton@gmail.com>
2023-06-23 17:02:13 +0300

    Update jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-undef */
-// Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
-    '\\.css$': '<rootDir>/emptyModule.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/mocks/emptyModule.js',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = {
+  ...customJestConfig,
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.js'],
+};
